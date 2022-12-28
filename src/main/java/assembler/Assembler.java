fc3998efb46a38e2f66f65320b03b3ec29e6a572
OKOOI <jyslovly1993@gmail.com>
2022-12-28 18:13:07 +0900

    2022-12-28

--- src/main/java/assembler/Assembler.java
+++ src/main/java/assembler/Assembler.java
@@ -1,20 +1,21 @@
 package assembler;
 
+import spring.CachedMemberDao;
 import spring.ChangePasswordService;
 import spring.MemberDao;
 import spring.MemberRegisterService;
 
-public class Assembler {
+public class Assembler{
 	// 실행에 필요한 객체를 필드로 정의 
 	private MemberDao memberDao;
 	private MemberRegisterService regSvc;
 	private ChangePasswordService pwdSvc;
 	
 	public Assembler() {
-		memberDao = new MemberDao();
+		memberDao = new CachedMemberDao();
 		regSvc = new MemberRegisterService(memberDao);
 		pwdSvc = new ChangePasswordService();
-		pwdSvc.setMemberDao(memberDao);
+//		pwdSvc.setMemberDao(memberDao);
 	}
 	
 	public MemberDao getMemberDao() {
